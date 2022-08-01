7bfb0674a7f68f2ea3475dd45045c2a7eb7d2b98
RosenbergYehuda <yehudazevr@gmail.com>
2022-08-01 14:39:58 +0300

    initial

---
+++ Algorithmic skills/first_unique.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def first_unique(A):
+    # Counting the items, keep only those which have a count of 1:
+    all_unique = [k for k, v in Counter(A).items() if v == 1]
+    if len(all_unique) == 0:
+        return -1
+    return all_unique[0]
+
+
+if __name__ == '__main__':
+    A = [2, 2, 3, 4, 5, 5]
+    print(first_unique(A))
